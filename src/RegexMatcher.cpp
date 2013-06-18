770d35d4fc71191f91965a37ced79b21fb5aeb96
airtrack <superking.airtrack@gmail.com>
2013-06-18 22:29:48 +0800

    Implement RegexMatcher::Match.

--- src/RegexMatcher.cpp
+++ src/RegexMatcher.cpp
@@ -1,4 +1,5 @@
 #include "RegexMatcher.h"
+#include <assert.h>
 
 namespace regex
 {
@@ -9,6 +10,24 @@ namespace regex
 
     bool RegexMatcher::Match(const char *begin, const char *end) const
     {
-        return false;
+        auto current_state = regex_.GetStartState();
+        assert(current_state);
+
+        for (auto current = begin; current != end; ++current)
+        {
+            // If *current is not belongs to the char set of
+            // regex_, then match fail.
+            auto next = regex_.GetNextStateIndex(*current);
+            if (!next.first)
+                return false;
+
+            // If next state is nullptr, then match fail.
+            current_state = current_state->next_[next.second];
+            if (!current_state)
+                return false;
+        }
+
+        // If current_state is accept state, then match success.
+        return current_state->accept_;
     }
 } // namespace regex
--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,15 @@
 #include "Regex.h"
+#include "RegexMatcher.h"
 #include "RegexException.h"
 #include <iostream>
 #include <assert.h>
+#include <string.h>
 
 int main(int argc, const char **argv)
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        std::cout << "usage: " << argv[0] << " regex_str" << std::endl;
+        std::cout << "usage: " << argv[0] << " regex_str match_str" << std::endl;
         return 0;
     }
 
@@ -15,6 +17,12 @@ int main(int argc, const char **argv)
     {
         regex::Regex re(argv[1]);
         std::cout << "regex \"" << argv[1] << "\" is ok." << std::endl;
+
+        regex::RegexMatcher matcher(re);
+        std::size_t len = strlen(argv[2]);
+        bool match = matcher.Match(argv[2], argv[2] + len);
+        std::cout << "\"" << argv[2] << "\"" << " match " << "\"" << argv[1] << "\" "
+                  << (match ? "ok" : "fail") << "." << std::endl;
     } catch (const regex::ParseException &e)
     {
         std::cout << "Parse error at index " << e.Position() << " :" << e.What() << std::endl;
